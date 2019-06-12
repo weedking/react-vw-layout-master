0ad72421db85f9b826f553536107a445ffc41364
weedking <260815998@qq.com>
2019-06-12 16:04:57 +0800

    移动web

---
+++ build/precache-manifest.f320c8db3c049a09d535a7b12b332163.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3a1ea2a41b510061e165",
+    "url": "./static/js/main.ddb7283c.chunk.js"
+  },
+  {
+    "revision": "2cabbd41b599e3fe4248",
+    "url": "./static/js/2.702a8299.chunk.js"
+  },
+  {
+    "revision": "3a1ea2a41b510061e165",
+    "url": "./static/css/main.f49de237.chunk.css"
+  },
+  {
+    "revision": "d40dc905953b5dbe7babf569117d2ec9",
+    "url": "./index.html"
+  }
+];
